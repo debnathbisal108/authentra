706517d578641d4340e5218fc3a15e6993051265
debnathbisal108 <debnathbisal108@gmail.com>
2026-06-08 00:02:06 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,16 +5,13 @@ const nextConfig = {
   async rewrites() {
     let backendUrl =
       process.env.API_INTERNAL_URL ||
-      process.env.NEXT_PUBLIC_API_URL ||
-      "http://backend:8000";
+      "https://authentra-oeft.onrender.com";
 
-    console.log("API_INTERNAL_URL =", process.env.API_INTERNAL_URL);
-    console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
-    console.log("backendUrl =", backendUrl);
-    
     if (backendUrl && !backendUrl.startsWith("http")) {
       backendUrl = `http://${backendUrl}`;
     }
+
+    console.log("[next.config] Proxying /api/* to:", backendUrl);
 
     return [
       {
