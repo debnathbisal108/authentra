ca1b658cea80b608e14b0a3aca89bdabf9d4edd2
debnathbisal108 <debnathbisal108@gmail.com>
2026-06-07 23:48:22 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_API_URL ||
       "http://backend:8000";
 
+    console.log("API_INTERNAL_URL =", process.env.API_INTERNAL_URL);
+    console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
+    console.log("backendUrl =", backendUrl);
+    
     if (backendUrl && !backendUrl.startsWith("http")) {
       backendUrl = `http://${backendUrl}`;
     }
