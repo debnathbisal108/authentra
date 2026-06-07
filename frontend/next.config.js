30c08206a98e401118ec39d8f18ebec1bc9e56ce
debnathbisal108 <debnathbisal108@gmail.com>
2026-06-07 21:18:43 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ??
-  "http://localhost:8000";
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000"],
-    },
-  },
+
   async rewrites() {
-    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+    let backendUrl =
+      process.env.API_INTERNAL_URL ||
+      process.env.NEXT_PUBLIC_API_URL ||
+      "http://backend:8000";
+
+    if (backendUrl && !backendUrl.startsWith("http")) {
+      backendUrl = `http://${backendUrl}`;
+    }
+
     return [
       {
         source: "/api/:path*",
-        // destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        destination: `${API_URL}/api/:path*`
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
